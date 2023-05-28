57542f2e6e44cc2de2c8243cc505557ef76f1fdb
Najeeb Arif <NajeebArif786@outlook.com>
2023-05-28 22:17:08 +0530

    Create DataConfig.java

---
+++ src/main/java/narif/poc/reactivebooks/config/DataConfig.java
@@ -0,0 +1,9 @@
+package narif.poc.reactivebooks.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+
+@Configuration
+@EnableR2dbcAuditing
+public class DataConfig {
+}
