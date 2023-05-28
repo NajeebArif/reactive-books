7472692a382cf49fd0b15e7f704867db2ae8d063
Najeeb Arif <NajeebArif786@outlook.com>
2023-05-28 23:20:12 +0530

    Add final code

--- src/main/java/narif/poc/reactivebooks/controllers/BookController.java
+++ src/main/java/narif/poc/reactivebooks/controllers/BookController.java
@@ -1,19 +1,41 @@
 package narif.poc.reactivebooks.controllers;
 
 import jakarta.validation.Valid;
+import narif.poc.reactivebooks.model.Book;
 import narif.poc.reactivebooks.model.BookDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import narif.poc.reactivebooks.repository.BookRepository;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @RestController
 @RequestMapping("books")
 public class BookController {
 
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @PostMapping
     public Mono<BookDTO> createBook(@RequestBody @Valid Mono<BookDTO> bookDTO){
-        return bookDTO;
+        return bookDTO.map(Book::of)
+                .flatMap(bookRepository::save)
+                .map(BookDTO::of)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @GetMapping
+    public Flux<BookDTO> fetchAllBooks(){
+        return bookRepository.findAll()
+                .map(BookDTO::of)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @PutMapping("/{isbn}")
+    public Mono<Integer> updatePrice(@PathVariable String isbn, @RequestParam Double price ){
+        return bookRepository.setPriceForIsbn(price, isbn);
     }
 }
--- src/main/java/narif/poc/reactivebooks/repository/BookRepository.java
+++ src/main/java/narif/poc/reactivebooks/repository/BookRepository.java
@@ -1,9 +1,16 @@
 package narif.poc.reactivebooks.repository;
 
 import narif.poc.reactivebooks.model.Book;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface BookRepository extends ReactiveCrudRepository<Book, Long> {
+
+    @Modifying
+    @Query("UPDATE books SET price = :price where isbn = :isbn")
+    Mono<Integer> setPriceForIsbn(Double price, String isbn);
 }
